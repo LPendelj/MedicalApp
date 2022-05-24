faefc1d8efef74e3de152f47b36f50847a6dca4f
LPendelj <pendeljluka@gmail.com>
2022-05-24 16:51:23 +0200

    initial commit

---
+++ Luka-Pendelj-BE/src/main/java/it/eng/lukapendelj/LukaPendeljBeApplication.java
@@ -0,0 +1,13 @@
+package it.eng.lukapendelj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukaPendeljBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukaPendeljBeApplication.class, args);
+	}
+
+}
---
+++ Luka-Pendelj-BE/src/test/java/it/eng/lukapendelj/LukaPendeljBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.eng.lukapendelj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukaPendeljBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
